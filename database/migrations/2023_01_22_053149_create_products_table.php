82c8319ac58a355b3e9a7632faec27bb2d61a2c3
wealthydeveloper <idongesitutong.com>
2023-01-22 07:57:34 +0100

    Passport install

---
+++ database/migrations/2023_01_22_053149_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->integer('unit_price')->nullable();
+            $table->bigInteger('amount');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
