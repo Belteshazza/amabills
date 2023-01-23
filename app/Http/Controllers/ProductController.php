925ca7a0ab7d94f8c2ccced22ed432c1326265cc
wealthydeveloper <idongesitutong.com>
2023-01-23 09:53:03 +0100

    added update feature

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -36,8 +36,42 @@ class ProductController extends Controller
 
         return response()->json([
             'message' => 'Congratulations You have Successfully created a product',
-            'appointment' => $product
+            'product' => $product
         ]);
+
+    }
+
+    public function updateProduct(Request $request, $id){
+
+        $request->validate([
+            'name' => ['required', 'string'],
+            'description' => ['required', 'string'],
+            'quantity' => ['required', 'int'],
+            'unit_price' => ['required', 'int'],
+            'amount' => ['required', 'int']
+        ]);
+
+        $product = Product::where('id', $id)->first();
+
+        if(auth()->user()->id === $product->user_id){        
+
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->quantity = $request->quantity;
+        $product->unit_price = $request->unit_price;
+        $product->amount = $request->amount;
+
+        $product->save();
+        
+        return response()->json([
+            'message' => 'Congratulations You have Successfully updated a product',
+            'data' => $product,
+        ]);
+
+        }else {
+
+            return response()->json(['message'=>'You do not own this product,so can not update it'], 401);
+        }
 
     }
 }
