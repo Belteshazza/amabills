f6b28d63f1b0d9e2a612a4b790bcac00420e492d
wealthydeveloper <idongesitutong.com>
2023-01-23 04:24:13 +0100

    user registration and login

---
+++ app/Http/Controllers/AuthUserController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthUserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                Rule::unique(User::class),
+            ],
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        try{
+            // create new user and save
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                
+            ]);
+                
+            return response()->json([
+                'message' => 'Registration Successful',
+                // 'token' => $user->createToken($request->name)->plainTextToken
+                'token' => $user->createToken($user->name)->accessToken,
+                'User' => $user
+            ]);
+        }catch(\Exception $e){
+            return response()->json($e->getMessage() . ' on line '  . $e->getLine(), 500);
+        }
+    }
+
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $email = strtolower($request->email);
+        $user = User::where('email', $email)->first();
+        if(empty($user))
+        {
+            $user = User::where('id', $email)->first();
+            if (empty($user)) 
+            {
+                throw ValidationException::withMessages([
+                    'email' => ['The provided credentials are incorrect.'],
+                ]);
+            } else {
+                $user = User::where('email', $user->email)->first();
+            }
+        }
+        $user = User::where('id', $user->id)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'password' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+            return [
+                'message' => 'Login Successful',   
+                'data' => [
+                'token' => $user->createToken($user->name)->accessToken,
+                    'user' => [
+                        'name' => $user->name,
+                        'email' => $user->email,
+                        'email_verified_at' => $user->email_verified_at,
+                        'created_at' => $user->created_at,
+                        'updated_at' => $user->updated_at,
+                    ],
+                ]
+            ];
+            
+                    
+
+}
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/register', [AuthUserController::class, 'register']);
+
+Route::post('/login', [AuthUserController::class, 'login']);
+
